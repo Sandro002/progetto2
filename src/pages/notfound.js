9cfdb3dd6ab8993d3782bb4ca6115011a1e59e3a
Sandro Luzzi <sandro.luzzi@mpolo.it>
2021-02-13 11:51:16 +0100

    gatto che piange

--- src/pages/notfound.js
+++ src/pages/notfound.js
@@ -1,14 +1,15 @@
 import { FormatAlignCenter, FormatAlignLeft } from '@material-ui/icons';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import logo from '../img/crying.jpg';
 export default function NotFound()
 {
     return(
         <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-            
-        <h1>ERROR 404-not found :(</h1>
+        <table>
+         <ul><h1>ERROR 404-not found :(</h1></ul>
+        <ul><img src={logo} className="App-logo" alt="logo" style={{ width: "800px", height: "450px" }} /></ul>
+        </table>
         </div>
     );
-
 }
