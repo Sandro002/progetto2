46a518d20d53219ab61333b9e5e3f6ca5a4878b4
Sandro Luzzi <sandro.luzzi@mpolo.it>
2021-02-13 11:09:16 +0100

    agginta pagina not found

---
+++ src/pages/notfound.js
@@ -0,0 +1,14 @@
+import { FormatAlignCenter, FormatAlignLeft } from '@material-ui/icons';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function NotFound()
+{
+    return(
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+            
+        <h1>ERROR 404-not found :(</h1>
+        </div>
+    );
+
+}
